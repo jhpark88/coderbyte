bd3efdb766dbe0f6dd1574140efe02a41d2f8765
jhpark <saskae88@gmail.com>
2015-07-28 15:34:55 +0900

    Create palindrome.rb

---
+++ palindrome.rb
@@ -0,0 +1,12 @@
+def Palindrome(str)
+
+  test_str = str.downcase.delete " "
+  flag = "false"
+  flag = "true" if test_str == test_str.reverse
+  return flag
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+Palindrome(STDIN.gets)
