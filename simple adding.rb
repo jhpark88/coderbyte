28daade38e9c270f53ba752b98ca8fc5963a654e
jhpark <saskae88@gmail.com>
2015-07-28 13:58:38 +0900

    Create simple adding.rb

---
+++ simple adding.rb
@@ -0,0 +1,14 @@
+def SimpleAdding(num)
+
+  # code goes here
+  
+    total = 0
+    1.upto(num) do |number|
+        total +=  number    
+    end
+    return total
+
+  
+  return num 
+         
+end
